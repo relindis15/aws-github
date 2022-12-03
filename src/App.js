234665500718ad18247e0ea5891d82a52b532644
relindis15 <onarirelindisblessed@gmail.com>
2022-12-02 21:57:00 -0500

    modified the application

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
      <h1>Welcome to compudemy</h1>
     <p>My name is Relindis
       I am a big Devops Engineer with over one year experience designing scalable<br></br>
-      applications within AWS cloud environment
+      applications within AWS cloud environment.I will be happy to work with you
     </p>
     </div>
   );
