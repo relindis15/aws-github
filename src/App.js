2df8c918b07ac0ac3e07c68cfed9d64fcdbd6c91
relindis15 <onarirelindisblessed@gmail.com>
2022-12-02 21:41:11 -0500

    cicd pipeline with github actions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ function App() {
   return (
     <div className="App">
      <h1>Welcome to compudemy</h1>
+    <p>My name is Relindis
+      I am a big Devops Engineer with over one year experience designing scalable<br></br>
+      applications within AWS cloud environment
+    </p>
     </div>
   );
 }
